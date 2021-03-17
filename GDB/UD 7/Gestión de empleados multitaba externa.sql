e7acaa8d9edc13d827a8f361d35e970a9fae61e9
Franco <77828917+FSP-1@users.noreply.github.com>
2021-03-17 08:56:25 +0100

    Update Gestión de empleados multitaba externa.sql

--- GDB/UD 7/Gestión de empleados multitaba externa.sql
+++ GDB/UD 7/Gestión de empleados multitaba externa.sql
@@ -50,12 +50,12 @@ ORDER BY 1 ASC;
 SELECT departamento.nombre, CONCAT_WS(' ',empleado.apellido1, empleado.apellido2, empleado.nombre) AS empleado
 FROM empleado LEFT JOIN   departamento
 ON empleado.codigo_departamento=departamento.codigo
-WHERE empleado.codigo_departamento IS NULL OR departamento.codigo IS NOT NULL
+WHERE departamento.codigo IS NOT NULL
 UNION
 SELECT departamento.nombre, CONCAT_WS(' ',empleado.apellido1, empleado.apellido2, empleado.nombre) AS empleado
 FROM departamento LEFT JOIN  empleado
 ON empleado.codigo_departamento=departamento.codigo
-WHERE empleado.codigo_departamento IS NULL OR departamento.codigo IS NOT NULL
+WHERE departamento.codigo IS NOT NULL
 ORDER BY 1 ASC;
  
 /*5*/
