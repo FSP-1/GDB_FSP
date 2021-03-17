5dd6573329cdcc3507bdb13fd7438a7810af0fea
Franco <77828917+FSP-1@users.noreply.github.com>
2021-03-17 08:57:45 +0100

    Update Gestión de empleados multitaba externa.sql

--- GDB/UD 7/Gestión de empleados multitaba externa.sql
+++ GDB/UD 7/Gestión de empleados multitaba externa.sql
@@ -39,23 +39,19 @@ WHERE empleado.codigo_departamento IS NULL;
 SELECT departamento.nombre, CONCAT_WS(' ',empleado.apellido1, empleado.apellido2, empleado.nombre) AS empleado
 FROM departamento RIGHT JOIN  empleado
 ON empleado.codigo_departamento=departamento.codigo
-WHERE empleado.codigo_departamento IS NULL OR departamento.codigo IS NOT NULL
 UNION
 SELECT departamento.nombre, CONCAT_WS(' ',empleado.apellido1, empleado.apellido2, empleado.nombre) AS empleado
 FROM empleado RIGHT JOIN departamento
 ON empleado.codigo_departamento=departamento.codigo
-WHERE empleado.codigo_departamento IS NULL OR departamento.codigo IS NOT NULL
 ORDER BY 1 ASC;
 /*LEFT*/
 SELECT departamento.nombre, CONCAT_WS(' ',empleado.apellido1, empleado.apellido2, empleado.nombre) AS empleado
 FROM empleado LEFT JOIN   departamento
 ON empleado.codigo_departamento=departamento.codigo
-WHERE departamento.codigo IS NOT NULL
 UNION
 SELECT departamento.nombre, CONCAT_WS(' ',empleado.apellido1, empleado.apellido2, empleado.nombre) AS empleado
 FROM departamento LEFT JOIN  empleado
 ON empleado.codigo_departamento=departamento.codigo
-WHERE departamento.codigo IS NOT NULL
 ORDER BY 1 ASC;
  
 /*5*/
