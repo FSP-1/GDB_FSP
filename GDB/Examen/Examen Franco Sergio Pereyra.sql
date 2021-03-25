a9a4cb8d4f3804aba5597f14b907da30a7baa19a
Franco <77828917+FSP-1@users.noreply.github.com>
2021-03-25 09:00:40 +0100

    Update Examen Franco Sergio Pereyra.sql

--- GDB/Examen/Examen Franco Sergio Pereyra.sql
+++ GDB/Examen/Examen Franco Sergio Pereyra.sql
@@ -1,12 +1,14 @@
 -- Universidad Tipo B -- 
 USE universidad;
 /*1*/
-SELECT asignatura.nombre,grado.nombre, COUNT(asignatura.id) as total_asignaturas
+SELECT asignatura.nombre, grado.nombre, COUNT(alumno_se_matricula_asignatura.id_alumno)
 FROM grado INNER JOIN asignatura
 ON grado.id = asignatura.id_grado
-GROUP BY grado.nombre 
-HAVING COUNT(asignatura.id) > 3
-ORDER BY asignatura.nombre ASC;
+INNER JOIN alumno_se_matricula_asignatura
+ON asignatura.id = alumno_se_matricula_asignatura.id_asignatura
+GROUP BY asignatura.id
+HAVING COUNT(alumno_se_matricula_asignatura.id_alumno) > 3
+ORDER BY asignatura.nombre;
 
 /*2*/
 SELECT   profesor.sexo, COUNT(profesor.sexo) as total_profesores
@@ -16,13 +18,11 @@ WHERE asignatura.id_profesor IS NULL
 GROUP BY profesor.sexo;
 
 /*3*/
-SELECT grado.nombre, alumno_se_matricula_asignatura.id_curso_escolar, asignatura.cuatrimestre, COUNT(asignatura.id) as total_asignaturas
-FROM grado  INNER JOIN  asignatura
+SELECT grado.nombre, asignatura.curso, asignatura.cuatrimestre,  COUNT(asignatura.id)
+FROM grado INNER JOIN asignatura
 ON grado.id = asignatura.id_grado
-INNER JOIN alumno_se_matricula_asignatura
-ON asignatura.id = alumno_se_matricula_asignatura.id_asignatura
-GROUP BY grado.id ,alumno_se_matricula_asignatura.id_curso_escolar,asignatura.cuatrimestre 
-ORDER BY 1 ASC,2 ASC ,3 ASC;
+GROUP BY grado.id, asignatura.curso, asignatura.cuatrimestre
+ORDER BY 1, 2, 3 ASC;
 
 -- Jardineria  -- 
 USE jardineria;
@@ -38,34 +38,39 @@ ORDER BY importe_total  desc
 Limit 0,4;
 
 /*5*/
-SELECT cliente.nombre_cliente,cliente.codigo_cliente,cliente.limite_credito,pago.total
+SELECT cliente.codigo_cliente, cliente.nombre_cliente, cliente.limite_credito, SUM(pago.total)
 FROM cliente INNER JOIN pago
-ON cliente.codigo_cliente  = pago.codigo_cliente
-WHERE pago.fecha_pago LIKE '2006%' 
-GROUP BY cliente.codigo_cliente,pago.total
-HAVING cliente.limite_credito > pago.total;
+ON cliente.codigo_cliente = pago.codigo_cliente
+WHERE YEAR(fecha_pago) = 2006
+GROUP BY cliente.codigo_cliente
+HAVING cliente.limite_credito > SUM(pago.total);
 
 /*6*/
-SELECT cliente.ciudad,COUNT(pedido.codigo_pedido),SUM(detalle_pedido.precio_unidad * detalle_pedido.cantidad) as importe_total_maximo,SUM(detalle_pedido.precio_unidad * detalle_pedido.cantidad) as importe_total_minimo
-FROM cliente INNER JOIN pedido
-ON cliente.codigo_cliente  = pedido.codigo_cliente
-INNER JOIN detalle_pedido
-ON pedido.codigo_pedido = detalle_pedido.codigo_pedido
-GROUP BY cliente.ciudad;
+SELECT ciudad, COUNT(codigo_pedido), MAX(total_pedido), MIN(total_pedido)
+FROM (
+    SELECT 
+        cliente.ciudad AS ciudad, 
+        pedido.codigo_pedido AS codigo_pedido, 
+        SUM(detalle_pedido.cantidad * detalle_pedido.precio_unidad) AS total_pedido
+    FROM cliente INNER JOIN pedido
+    ON cliente.codigo_cliente = pedido.codigo_cliente
+    INNER JOIN detalle_pedido
+    ON pedido.codigo_pedido = detalle_pedido.codigo_pedido
+    GROUP BY cliente.ciudad, pedido.codigo_pedido) AS t
+GROUP BY ciudad
+ORDER BY 2 DESC;
 
 -- Netflix  -- 
 USE sakila_es;
 /*7*/
-SELECT cliente.nombre,cliente.apellidos,direccion.telefono,pago.total,COUNT(alquiler.id_alquiler)
+SELECT cliente.nombre, cliente.apellidos, direccion.telefono, SUM(pago.total), COUNT(pago.id_alquiler)
 FROM cliente INNER JOIN direccion
 ON cliente.id_direccion = direccion.id_direccion
 INNER JOIN pago
 ON cliente.id_cliente = pago.id_cliente
-INNER JOIN alquiler
-ON pago.id_alquiler = alquiler.id_alquiler
-GROUP BY cliente.id_cliente,pago.total
-HAVING COUNT(alquiler.id_alquiler) > 15
-ORDER BY COUNT(alquiler.id_alquiler) DESC;
+GROUP BY cliente.id_cliente
+HAVING COUNT(pago.id_alquiler) > 15
+ORDER BY 4 DESC;
 
 /*8*/
 SELECT pelicula.titulo,COUNT(alquiler.id_alquiler)
@@ -74,20 +79,22 @@ ON pelicula.id_pelicula = inventario.id_pelicula
 INNER JOIN alquiler
 ON inventario.id_inventario = alquiler.id_inventario
 GROUP BY pelicula.titulo
+HAVING COUNT(alquiler.id_alquiler) > 30
 ORDER BY COUNT(alquiler.id_alquiler) DESC;
 
+
 /*9*/
-SELECT cliente.nombre,cliente.apellidos,categoria.nombre,COUNT(alquiler.id_alquiler)
+SELECT cliente.nombre, cliente.apellidos, categoria.nombre, COUNT(pelicula.id_pelicula)
 FROM cliente INNER JOIN alquiler
 ON cliente.id_cliente = alquiler.id_cliente
-INNER JOIN inventario 
+INNER JOIN inventario
 ON inventario.id_inventario = alquiler.id_inventario
-INNER JOIN pelicula 
+INNER JOIN pelicula
 ON pelicula.id_pelicula = inventario.id_pelicula
 INNER JOIN pelicula_categoria
-ON pelicula.id_pelicula = pelicula_categoria.id_pelicula
+ON pelicula_categoria.id_pelicula =pelicula.id_pelicula
 INNER JOIN categoria
-ON pelicula_categoria.id_categoria = categoria.id_categoria
-WHERE DATE_FORMAT(alquiler.fecha_alquiler,'%Y') = 2005
-GROUP BY cliente.id_cliente,categoria.nombre;
+ON categoria.id_categoria = pelicula_categoria.id_categoria
+WHERE YEAR(alquiler.fecha_alquiler) = 2005 AND MONTH(alquiler.fecha_alquiler) = 5
+GROUP BY cliente.id_cliente, categoria.id_categoria;
 
