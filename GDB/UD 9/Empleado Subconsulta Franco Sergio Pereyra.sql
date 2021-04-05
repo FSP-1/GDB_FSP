593466a1857c68bb76c62796aa0ba53775834ee1
Franco <77828917+FSP-1@users.noreply.github.com>
2021-04-05 08:31:29 +0200

    Update Empleado Subconsulta Franco Sergio Pereyra.sql

--- GDB/UD 9/Empleado Subconsulta Franco Sergio Pereyra.sql
+++ GDB/UD 9/Empleado Subconsulta Franco Sergio Pereyra.sql
@@ -4,7 +4,7 @@ SELECT *
 FROM empleado
 WHERE empleado.codigo_departamento = (SELECT departamento.codigo
         FROM departamento
-        WHERE  empleado.codigo_departamento = departamento.codigo AND departamento.nombre = 'Sistemas');
+        WHERE  departamento.nombre = 'Sistemas');
         
  /*2*/       
 SELECT *
@@ -16,5 +16,60 @@ WHERE presupuesto = (SELECT MAX(presupuesto)
 SELECT *
 FROM departamento
 WHERE presupuesto = (SELECT MIN(presupuesto) 
-     FROM departamento);     
+     FROM departamento);
+     
+/*Subconsultas con ALL y ANY*/
+ /*4*/       
+SELECT *
+FROM departamento
+WHERE presupuesto >= ALL (SELECT presupuesto
+     FROM departamento);
+
+ /*5*/       
+SELECT *
+FROM departamento
+WHERE presupuesto <= ALL (SELECT presupuesto
+     FROM departamento);
+
+ /*6*/       
+SELECT nombre
+FROM departamento
+WHERE codigo = ANY (SELECT codigo_departamento
+     FROM empleado);     
+     
+ /*7*/       
+SELECT nombre
+FROM departamento
+WHERE codigo != ALL (SELECT empleado.codigo_departamento
+     FROM empleado
+     WHERE codigo_departamento IS NOT NULL); 
+     
+/*Subconsultas con IN y NOT IN*/
+ /*8*/       
+SELECT nombre
+FROM departamento
+WHERE codigo IN (SELECT codigo_departamento
+     FROM empleado);     
+     
+ /*9*/       
+SELECT nombre
+FROM departamento
+WHERE codigo NOT IN  (SELECT codigo_departamento
+     FROM empleado
+     WHERE codigo_departamento IS NOT NULL); 
+     
+/*Subconsultas con EXISTS y NOT EXISTS*/
+ /*10*/       
+SELECT nombre
+FROM departamento
+WHERE EXISTS (SELECT codigo_departamento
+     FROM empleado
+     WHERE empleado.codigo_departamento = departamento.codigo);     
+     
+ /*11*/       
+SELECT nombre
+FROM departamento
+WHERE  NOT EXISTS  (SELECT codigo_departamento
+     FROM empleado
+     WHERE empleado.codigo_departamento = departamento.codigo AND codigo_departamento IS NOT NULL); 
      
