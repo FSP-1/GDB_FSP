bde4f338b9871e8dd5776c35dd8e4f9bb29c6eaf
Franco <77828917+FSP-1@users.noreply.github.com>
2021-04-16 12:49:58 +0200

    Update YMHNJU.sql

--- GDB/UD 10/YMHNJU.sql
+++ GDB/UD 10/YMHNJU.sql
@@ -10,19 +10,96 @@ SELECT AVG(total)
 FROM pago
 WHERE fecha_pago >= '2010-01-01' AND fecha_pago <= '2010-12-31';CREATE INDEX idx_year ON pago(total);
 SHOW INDEX FROM pago;
+/*EjemploNº1*/
+EXPLAIN
+SELECT nombre_contacto, telefono
+FROM cliente
+WHERE pais = 'France';
+ 
+Show index from cliente;
 
+DESCRIBE
+cliente;
 
+CREATE INDEX idx_pais ON cliente(pais);
+
+/*EjemploNº2*/
 CREATE FULLTEXT INDEX inx_nombre_descripcion ON producto(nombre,descripcion);
 
  show index from producto;
 
   DROP INDEX inx_nombre_descripcion ON producto;
-
-Explain
+  
+/*7.1*/
+-- 5 --
 SELECT *
 FROM producto
-WHERE MATCH (nombre,descripcion) AGAINST ('A*' IN BOOLEAN MODE);
-
-EXPLAIN SELECT *
+WHERE MATCH (nombre,descripcion) AGAINST ('planta' IN NATURAL LANGUAGE MODE);
+-- 5 --
+SELECT *
 FROM producto
-WHERE nombre LIKE '%semilla%' OR descripcion LIKE '%semilla%';
+WHERE MATCH (nombre,descripcion) AGAINST ('planta' IN BOOLEAN MODE);
+-- 131 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('planta' WITH QUERY EXPANSION);
+
+/*7.2*/
+-- 17 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('planta*' IN BOOLEAN MODE);
+
+/*7.3*/
+-- 1 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('planta*' IN BOOLEAN MODE)
+AND (descripcion LIKE 'planta%' OR nombre LIKE 'planta%');
+/*7.4*/
+-- 80 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('tronco arbol' IN NATURAL LANGUAGE MODE);
+-- 80 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('tronco arbol' IN BOOLEAN MODE);
+-- 158 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('tronco arbol' WITH QUERY EXPANSION);
+
+/*7.5*/
+-- 80 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('+tronco +arbol' IN NATURAL LANGUAGE MODE);
+-- 40 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('+tronco +arbol' IN BOOLEAN MODE);
+-- 158 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('+tronco +arbol' WITH QUERY EXPANSION);
+
+/*7.6*/
+-- 7 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('+tronco -arbol' IN BOOLEAN MODE);
+
+/*7.7*/
+-- 5 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('"proviene de las costas"' IN NATURAL LANGUAGE MODE);
+-- 5 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('"proviene de las costas"' IN BOOLEAN MODE);
+-- 120 --
+SELECT *
+FROM producto
+WHERE MATCH (nombre,descripcion) AGAINST ('"proviene de las costas"' WITH QUERY EXPANSION);
